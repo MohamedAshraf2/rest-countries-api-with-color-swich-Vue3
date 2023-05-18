7a5d897677528da009cc0f2510867e99bda18bf0
MohamedAshraf2 <hamoudy495@gmail.com>
2023-05-18 15:53:20 +0300

    try2

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+  publicPath: '/a',
+  outputDir: 'dist/a',
   productionSourceMap: false,  
-  outputDir: '../dist',
   assetsDir: 'static',
   indexPath: 'templates/index.html'
 })
